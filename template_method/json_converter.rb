86ab7fc65caa82eb2fc5479179cea37cdd55780e
naoko-asano <asano@wyrd.co.jp>
2023-05-04 18:30:33 +0900

    refactor: TemplateMethodパターンを1枚にまとめる

--- template_method/json_converter.rb
+++
@@ -1,8 +0,0 @@
-require 'json'
-require_relative 'file_converter'
-
-class JsonConverter < Converter
-  def convert
-    puts @data.to_json
-  end
-end
--- template_method/main.rb
+++ template_method/main.rb
@@ -1,5 +1,43 @@
-require_relative './json_converter'
-require_relative './yaml_converter'
+require 'json'
+require 'yaml'
+
+CONTENTS = { name: "Alice", age: 18 }.freeze
+
+# AbstractClass
+class Converter
+  # template method
+  def convert_with_message
+    start
+    convert
+    finish
+  end
+
+  def start
+    puts "開始します"
+  end
+
+  def convert
+    raise StandardError.new("サブクラスで定義して！")
+  end
+
+  def finish
+    puts "終了します"
+  end
+end
+
+# ConcreateClass
+class JsonConverter < Converter
+  def convert
+    puts CONTENTS.to_json
+  end
+end
+
+# ConcreateClass
+class YamlConverter < Converter
+  def convert
+    puts CONTENTS.to_yaml
+  end
+end
 
 JsonConverter.new.convert_with_message
 YamlConverter.new.convert_with_message
