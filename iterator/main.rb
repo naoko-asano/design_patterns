f61cb6ebdb6ad413ce5785bce55a3a9475d3dcd7
naoko-asano <asano@wyrd.co.jp>
2023-05-07 15:30:02 +0900

    feat: Iteratorパターンを修正

--- iterator/main.rb
+++ iterator/main.rb
@@ -1,4 +1,25 @@
-require_relative "./array_iterator"
+# 外部イテレータ
+class ArrayIterator
+  def initialize(array)
+    @array = Array.new(array)
+    @index = 0
+  end
+
+  def has_next?
+    @index < @array.length
+  end
+
+  def item
+    @array[@index]
+  end
+
+  def next_item
+    value = item
+    @index += 1
+    value
+  end
+end
+
 
 array = ["red", "blue", "green"]
 
